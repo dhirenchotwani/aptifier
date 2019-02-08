50a38f29c27c7340ed0b537e4d2fe2a3f149d3f4
Dhiren Chotwani <dhirenchotwani@gmail.com>
2019-02-09 00:54:05 +0530

    database as of now

--- quiz_handlers.sql
+++ quiz_handlers.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2019 at 08:19 PM
+-- Generation Time: Feb 08, 2019 at 08:23 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
